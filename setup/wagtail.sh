1c3bd0bc23afa65bd41084a8110a040b475f56e1
deltwith <aahughes21@gmail.com>
2020-01-18 23:15:05 -0500

    try this with a new server

--- setup/wagtail.sh
+++ setup/wagtail.sh
@@ -13,14 +13,15 @@ source /etc/mailinabox.conf # load global vars
 # Install packages.
 echo "Installing Wagtail, gunicorn, and postgresql..."
 
-hide_output pip3 install cookiecutter
+hide_output pip3 install cookiecutter build-essential postgresql-server-dev-all
 
 RETURN_TO=$(pwd)
 mkdir "$WAGTAIL_LOC" || echo ''
 cd "$WAGTAIL_LOC"
 
 #WAGTAIL_PROJ=$(ls -td -- */ | head -n 1)
-cd $WAGTAIL_PROJ
+#mkdir "$WAGTAIL_PROJ" || echo ''
+#
 
 mkdir ~/.cookiecutters/cookiecutter-django/ || echo ''
 git clone https://github.com/pydanny/cookiecutter-django ~/.cookiecutters/cookiecutter-django/ || echo ''
@@ -58,7 +59,7 @@ EOF
 
 #cookiecutter https://github.com/pydanny/cookiecutter-django
 cookiecutter ~/.cookiecutters/cookiecutter-django/
-
+cd $WAGTAIL_PROJ
 hide_output pip3 install -r requirements/production.txt
 
 cat > /etc/systemd/system/gunicorn.socket << EOF;
@@ -98,7 +99,7 @@ EOF
 cat > /etc/nginx/sites-available/huetest << EOF;
 server {
     listen 80;
-    localhost 155.138.238.79;
+    localhost %PRIMARY_HOSTNAME;
 
     location = /favicon.ico { access_log off; log_not_found off; }
     location /root/huetest/huetest/static/ {
@@ -112,7 +113,10 @@ server {
 }
 EOF
 '
+systemctl start gunicorn
+systemctl enable gunicorn
 restart_service gunicorn.socket
+systemctl restart nginx
 
 cd "$RETURN_TO" || exit
 
@@ -120,30 +124,4 @@ cd "$RETURN_TO" || exit
 
 
 
-
-
-
-
-
-
-
-
-
-
-# Enable the Dovecot antispam plugin.
-# (Be careful if we use multiple plugins later.) #NODOC
-sed -i "s/#mail_plugins = .*/mail_plugins = \$mail_plugins antispam/" /etc/dovecot/conf.d/20-imap.conf
-sed -i "s/#mail_plugins = .*/mail_plugins = \$mail_plugins antispam/" /etc/dovecot/conf.d/20-pop3.conf
-
-chmod a+x /usr/local/bin/sa-learn-pipe.sh
-
-# Create empty bayes training data (if it doesn't exist). Once the files exist,
-# ensure they are group-writable so that the Dovecot process has access.
-sudo -u spampd /usr/bin/sa-learn --sync 2>/dev/null
-chmod -R 660 $STORAGE_ROOT/mail/spamassassin
-chmod 770 $STORAGE_ROOT/mail/spamassassin
-
-# Kick services.
-restart_service spampd
-restart_service dovecot
 
